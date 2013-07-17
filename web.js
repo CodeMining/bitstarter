1ae6868dd880b2322c731acfd705f68779fe9499
Wayne Bontrager <wbontrager@live.com>
2013-07-17 01:59:45 +0000

    Updated web.js old

--- web.js
+++ web.js
@@ -6,7 +6,8 @@ var buffer = nwerar msg = buffer.topp.get('/', function(request, response) {
   var fs = require('fs');
   var buff2 = new Buffer();
 
-  response.send(buff2.toString('utc-8', fs.readFileSync("index.html")));
+//  response.send(buff2.toString('utc-8', fs.readFileSync("index.html")));
+  response.send("Hello World from index.html");
 });
 
 var port = process.env.PORT || 5000;
