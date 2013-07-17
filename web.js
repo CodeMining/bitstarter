9bc0ca8f780e56d22d5cda4358166b83e2f3e439
Wayne Bontrager <wbontrager@live.com>
2013-07-17 02:10:03 +0000

    Updated web.js 6

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   var fs = require('fs');
 //  var buff2 = new Buffer();
-  response.send(fs.readFileSync("index.html"));
+  response.send(fs.readFileSync("index.html").toString());
 //  response.send(buff2.toString('utc-8', fs.readFileSync("index.html")));
 //  response.send("Hello World from index.html");
 });
