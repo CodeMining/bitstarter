7b2ac0f6aeb4ed4cda2866457b4ada48fc657415
Wayne Bontrager <wbontrager@live.com>
2013-07-17 01:45:28 +0000

    Updated web.js 2

--- web.js
+++ web.js
@@ -2,8 +2,9 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send(fs.readFileSync(index.html);
+
+var msg = buffer.topp.get('/', function(request, response) {
+  response.send(fs.readFileSync('index.html');
 });
 
 var port = process.env.PORT || 5000;
