a6cff41a22f95bbf9dcf0792b12e9deeab97239d
Wayne Bontrager <wbontrager@live.com>
2013-07-20 22:41:31 +0000

    edited web.js for port 8080

--- web.js
+++ web.js
@@ -1,16 +1,15 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
+var htmlfile = "index.html";
 
 app.get('/', function(request, response) {
-  var fs = require('fs');
-//  var buffer = new Buffer();
-  response.send(fs.readFileSync("index.html").toString());
-//  response.send(buff2.toString('utc-8', fs.readFileSync("index.html")));
-//  response.send("Hello World from index.html");
+  var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
+
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
